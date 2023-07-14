98c6a1c721c218a43e9f59c778d0ab51215b15a9
Matheus Barbosa <87444720+souzera@users.noreply.github.com>
2023-07-14 16:14:08 -0300

    Delete license.tsx

--- web/src/connection/license.tsx
+++
@@ -1,6 +0,0 @@
-export const license = {
-    client_id: "aec91af126cb430f8aff39097b642be9",
-    redirect_uri: "http://localhost:5173/playlists",
-    scope: "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public",
-    client_secret: "42380d4bb21240ddb5fedfc62cb5ca33"
-}
