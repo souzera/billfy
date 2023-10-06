1b473d5dc0add517d25694bd36097ba576dbc7a6
unknown <towork.matheusb@gmail.com>
2023-10-06 12:05:24 -0300

    update redirect link alterando

--- web/src/connection/license.ts
+++ web/src/connection/license.ts
@@ -5,7 +5,7 @@ export const license = {
 
     client_id: "aec91af126cb430f8aff39097b642be9",
     //redirect_uri: "http://localhost:5173/playlists",
-    redirect_uri: "/playlist",
+    redirect_uri: "http://billfy.vercel.app/playlists",
     scope: "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public",
     client_secret: "42380d4bb21240ddb5fedfc62cb5ca33"
 
