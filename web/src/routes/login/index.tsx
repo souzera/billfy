1fa6f0ffa83b66faf8c65abdf206bd0dc22e5d9f
unknown <towork.matheusb@gmail.com>
2023-10-25 10:09:23 -0300

    config deploy

--- web/src/routes/login/index.tsx
+++ web/src/routes/login/index.tsx
@@ -1,6 +1,6 @@
 
 //import { useAuthContext } from "../../components/context/auth-context"
-import axios from "axios"
+//import axios from "axios"
 import { license } from "../../connection/license"
 import { Link } from "react-router-dom"
 
